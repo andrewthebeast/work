d7c99f584f8f2e7074e1f5f7d6967c86bbbc679c
andrewthebeast <andrewr.luna@yahoo.com>
2016-12-21 11:49:19 -0600

    Update FossilRelative.java

--- Luna_5_CryptoZoo/src/luna_5_cryptozoo/FossilRelative.java
+++ Luna_5_CryptoZoo/src/luna_5_cryptozoo/FossilRelative.java
@@ -8,10 +8,23 @@
  ********************************************************************************/
 package luna_5_cryptozoo;
 public class FossilRelative extends LingerLing {
-    //
+     /***************************************************************************
+     * Method: FossilRelative()
+     * Description: makes a class constructor
+     * Parameters: name, orgin, and dangerrating
+     * Pre-Conditions: none
+     * Post-Conditions: sets name, orgin and danger rating
+     **************************************************************************/
     public FossilRelative(String name, String origin, int dangerrating){
         super(name, origin, dangerrating);
     }
+        /***************************************************************************
+     * Method: foodperWeek()
+     * Description: sets the foodperweek
+     * Parameters: none
+     * Pre-Conditions: none
+     * Post-Conditions: sets foodperweek
+     **************************************************************************/
     public int foodperWeek(){
         return 1;
     }
