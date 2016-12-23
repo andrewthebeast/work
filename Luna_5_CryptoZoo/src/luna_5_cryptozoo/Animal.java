e0c38d936c7326d04023de087bc6507dc8d8e978
andrewthebeast <andrewr.luna@yahoo.com>
2016-12-22 18:47:46 -0600

    Update Animal.java

--- Luna_5_CryptoZoo/src/luna_5_cryptozoo/Animal.java
+++ Luna_5_CryptoZoo/src/luna_5_cryptozoo/Animal.java
@@ -9,10 +9,10 @@
 package luna_5_cryptozoo;
 import java.util.*;
 public abstract class Animal implements ZooAnimal{
-    private String name;
-    private String origin;
-    private int dangerrating;
-    private boolean onTour = false;
+    private String name;//sets the name of the creature
+    private String origin;//sets the orgin of the animal
+    private int dangerrating;// sets the danger rating of the animal
+    private boolean onTour = false;// sets if the animal is on tour
     /***************************************************************************
      * Method: Animal()
      * Description: makes a class constructor
@@ -25,7 +25,13 @@ public abstract class Animal implements ZooAnimal{
         this.origin = b;
         this.dangerrating = c;
     }
-    //
+    /***************************************************************************
+     * Method: toString()
+     * Description: returns the name orgin and dangerrating
+     * Parameters: name, orgin, and dangerrating
+     * Pre-Conditions: none
+     * Post-Conditions: returns name, orgin and danger rating
+     **************************************************************************/
     public String toString(){
         return "Animal : " + name + "\n Orgin : " + origin + "\n Danger Rating" + dangerrating;
     }
