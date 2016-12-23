fa63df8eecd8b6d1fbeecac15f53f6d7ef3d7750
andrewthebeast <andrewr.luna@yahoo.com>
2016-12-22 18:37:13 -0600

    Update FossilRelative.java

--- Luna_5_CryptoZoo/src/luna_5_cryptozoo/FossilRelative.java
+++ Luna_5_CryptoZoo/src/luna_5_cryptozoo/FossilRelative.java
@@ -2,9 +2,9 @@
  * Program Filename: FossilRelative.java
  * Author: Luna, Andrew
  * Date: December 19, 2016
- * Description:
- * Input:
- * Output:
+ * Description:creates a class constructor for Fossil Relative and sets the food per week
+ * Input:class lingerling
+ * Output: class constructor for fossil relative
  ********************************************************************************/
 package luna_5_cryptozoo;
 public class FossilRelative extends LingerLing {
