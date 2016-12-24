b7a08090eaa627598b48883be43b4f979a921fbc
andrewthebeast <andrewr.luna@yahoo.com>
2016-12-24 15:21:12 -0600

    Update Zoo.java

--- Luna_5_CryptoZoo/src/luna_5_cryptozoo/Zoo.java
+++ Luna_5_CryptoZoo/src/luna_5_cryptozoo/Zoo.java
@@ -3,8 +3,8 @@
  * Author: Luna, Andrew
  * Date: December 19, 2016
  * Description: Creates the creatures and shows them
- * Input:
- * Output:
+ * Input: animal and other classes
+ * Output: 
  ********************************************************************************/
 package luna_5_cryptozoo;
 import java.util.*;
@@ -20,21 +20,25 @@ public class Zoo {
      * Post-Conditions: none
      **************************************************************************/
     public static void list(){
-        Mythical animal1 = new Mythical("Fairy", "Woods", 0, "dreams", 20);
+        Mythical animal1 = new Mythical("Fairy", "Woods", 0, "dreams", 20);//1st animal
         creatures.add(animal1);
-        Mythical animal2 = new Mythical("Barney", "your dreams", 25, "songs", 100);
+        Mythical animal2 = new Mythical("Barney", "your dreams", 25, "songs", 100);// 2nd animal
         
-        Mythical animal3 = new Mythical("Whale", "under the sea", 10, "flying fish", 75);
+        Mythical animal3 = new Mythical("Whale", "under the sea", 10, "flying fish", 75);//3rd Animal
         
-        Paranormal animal4 = new Paranormal("", "", 0);
+        Paranormal animal4 = new Paranormal("Bigfoot", "forrest", 50);//4th animal
         
-        Paranormal animal5 = new Paranormal("", "", 0);
+        Paranormal animal5 = new Paranormal("Lizard Man", "Lab", 40);//5th animal
         
-        LingerLing animal6 = new LingerLing("", "", 0);
+        LingerLing animal6 = new LingerLing("Zombie", "graveyard", 35);//6th animal
         
-        LingerLing animal7 = new LingerLing("", "", 0);
+        LingerLing animal7 = new LingerLing("Vampire", "the dark", 28);//7th animal
         
-        LingerLing animal8 = new LingerLing("", "", 0);
+        LingerLing animal8 = new LingerLing("Werewolf", "", 1000);//8th animal
+        
+        FossilRelative animal9 = new FossilRelative("Dinosaur", "eggs", 15);//9th animal
+        
+        FossilRelative animal10 = new FossilRelative("Monkey", "the jungle", 20);//10th animal
         
     }
      /***************************************************************************
