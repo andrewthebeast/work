a63f6683c1e4c188412ca80064ee452d9b051b1d
andrewthebeast <andrewr.luna@yahoo.com>
2017-02-21 14:22:41 -0600

    Add files via upload

---
+++ Luna_5_TheGame/src/luna_5_thegame/Luna_5_TheGame.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Program Filename : thegame.java
+ * Author : Luna, Andrew
+ * Date : February 21, 2017
+ * Description : The class that will hold the 1st level of a puppy dash game.
+ * Input : user keyboard controls
+ * Output: tiled map sheet game
+ */
+package luna_5_thegame;
+//imports
+import java.util.ArrayList;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.Sound;
+import org.newdawn.slick.tiled.TiledMap;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.state.transition.FadeInTransition;
+import org.newdawn.slick.state.transition.FadeOutTransition;
+
+public class Luna_5_TheGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
