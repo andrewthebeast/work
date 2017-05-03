e71af395e2a2a5afca09494fc3710b8e9241df33
andrewthebeast <andrewr.luna@yahoo.com>
2017-05-03 14:25:38 -0500

    Update Luna_5_Vendingmachine.java

--- Luna_5_Vendingmachine/src/luna_5_vendingmachine/Luna_5_Vendingmachine.java
+++ Luna_5_Vendingmachine/src/luna_5_vendingmachine/Luna_5_Vendingmachine.java
@@ -15,8 +15,28 @@ public class Luna_5_Vendingmachine {
     static ArrayList<Candies> candies = new ArrayList<Candies>();
     public static void main(String[] args) {
         //candies to be used = skittles, snickers, hersheys, twix, almond joy, kit kat, starburst, m&ms, 100 grand
-        double money;
+        Candies candy1 = new Candies("Skittles", 8, 1.00, 170);
+        candies.add(candy1);
+        Candies candy2 = new Candies("Snickers", 8, 1.25, 280);
+        candies.add(candy2);
+        Candies candy3 = new Candies("Hersheys", 8, 1.00, 230);
+        candies.add(candy3);
+        Candies candy4 = new Candies("Twix", 8, .75, 286);
+        candies.add(candy4);
+        Candies candy5 = new Candies("Almond Joy", 8, .75, 243);
+        candies.add(candy5);
+        Candies candy6 = new Candies("Kit Kat", 8, 1.00, 218);
+        candies.add(candy6);
+        Candies candy7 = new Candies("Starburst", 8, .75, 160);
+        candies.add(candy7);
+        Candies candy8 = new Candies("M&M's", 8, 1.25, 230);
+        candies.add(candy8);
+        Candies candy9 = new Candies("100 Grand", 8, .90, 201);
+        candies.add(candy9);
         candiesMaker();
+    } 
+    public static void candiesMaker(){
+        double money;
         System.out.println("Welcome to the vending machine!");
         System.out.println("How much money do you have?");
         System.out.print("$ :");
@@ -25,28 +45,9 @@ public class Luna_5_Vendingmachine {
         System.out.println("Here is the vending machine");
         for (int i = 0; i < machine.length; i++) {
             for (int j = 0; j < machine.length; j++) {
-                System.out.print(candies);
+                System.out.print(candies.get(i).getName());
             }
         }
-    } 
-    public static void candiesMaker(){
-        Candies candy1 = new Candies("Skittles", 8, 1.00);
-        candies.add(candy1);
-        Candies candy2 = new Candies("Snickers", 8, 1.25);
-        candies.add(candy2);
-        Candies candy3 = new Candies("Hersheys", 8, 1.00);
-        candies.add(candy3);
-        Candies candy4 = new Candies("Twix", 8, .75);
-        candies.add(candy4);
-        Candies candy5 = new Candies("Almond Joy", 8, .75);
-        candies.add(candy5);
-        Candies candy6 = new Candies("Kit Kat", 8, 1.00);
-        candies.add(candy6);
-        Candies candy7 = new Candies("Starburst", 8, .75);
-        candies.add(candy7);
-        Candies candy8 = new Candies("M&M's", 8, 1.25);
-        candies.add(candy8);
-        Candies candy9 = new Candies("100 Grand", 8, .90);
-        candies.add(candy9);
+        
     }
 }
