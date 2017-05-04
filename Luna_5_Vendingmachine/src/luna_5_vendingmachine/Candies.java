c4fb2e34688886634fcdacf7e456e793eb4e673e
andrewthebeast <andrewr.luna@yahoo.com>
2017-05-04 14:22:50 -0500

    Update Candies.java

--- Luna_5_Vendingmachine/src/luna_5_vendingmachine/Candies.java
+++ Luna_5_Vendingmachine/src/luna_5_vendingmachine/Candies.java
@@ -1,21 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*******************************************************************************
+ * Program Filename : Candies.java
+ * Author : Luna, Andrew
+ * Date : April 18, 2017
+ * Description : class candies creates the array list candies
+ * Input : user
+ * Output: updated 
+ ******************************************************************************/
 package luna_5_vendingmachine;
-
-/**
- *
- * @author andrewthebeast
- */
 public class Candies {
     private String name;
     private int amt;
     private double prc;
-     public Candies(String a, int b, double c){
+    private int cals;
+    
+     public Candies(String a, int b, double c, int d){
         this.name = a;
         this.amt = b;
         this.prc = c;
+        this.cals = d;
      }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmt() {
+        return amt;
+    }
+
+    public void setAmt(int amt) {
+        this.amt = amt;
+    }
+
+    public double getPrc() {
+        return prc;
+    }
+
+    public void setPrc(double prc) {
+        this.prc = prc;
+    }
+
+    public int getCals() {
+        return cals;
+    }
+
+    public void setCals(int cals) {
+        this.cals = cals;
+    }
+     
 }
